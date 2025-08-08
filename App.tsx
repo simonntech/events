b266f5f2b240d0d8333ffb875a293fa0c45e23c8
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-08-08 20:29:50 -0300

    funções diferentes maneiras onPress

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { helloDarthVader } from './src/actions';
 
 export default function App() {
 
-  async function handlePress(id:number) {
+  async function handlePress(id: number) {
     console.log('Pressionado', id)
+  }
+
+  async function handlerHelloWorld() {
+    console.log('Hello handler function')
   }
 
   return (
     <View style={styles.container}>
       <Text
-        style={{fontSize:18}} 
-        onPress={() => handlePress(5)} 
-      >Open up App.tsx to start working on your app!</Text>
+        onPress={() => console.log('Inline')}
+        style={{ fontSize: 18 }}
+      >Inline</Text>
+      <Text
+        style={{ fontSize: 18 }}
+        onPress={handlerHelloWorld}
+      >Handler Function</Text>
+      <Text
+        style={{ fontSize: 18 }}
+        onPress={() => handlePress(5)}
+      >Handler Function with Parameters</Text>
+      <Text
+        style={{ fontSize: 18 }}
+        onPress={helloDarthVader}
+      >Another File Function</Text>
     </View>
   );
 }
---
+++ src/actions.ts
@@ -0,0 +1,3 @@
+export const helloDarthVader = () => {
+    console.log('Hello from another file')
+}
